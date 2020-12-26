0e3012da810ab8efad9f9b697250a00a493ec955
Nick Ramsay <NickRamsay1989@gmail.com>
2020-12-26 20:32:26 +1100

    Added accordion to value search to hide advanced option

--- client/src/pages/ValueSearch/ValueSearch.js
+++ client/src/pages/ValueSearch/ValueSearch.js
@@ -7,6 +7,9 @@ import { logout, useInput, getCookie } from "../../sharedFunctions/sharedFunctio
 import Navbar from "../../components/Navbar/Navbar";
 import AuthTimeoutModal from "../../components/AuthTimeoutModal/AuthTimeoutModal";
 
+import expandMoreIcon from "../../images/icons/baseline_expand_more_white_48dp.png";
+import expandLessIcon from "../../images/icons/baseline_expand_less_white_48dp.png";
+
 
 import API from "../../utils/API";
 
@@ -26,6 +29,7 @@ const fetchAllQuotes = () => {
 
 const ValueSearch = () => {
 
+    var [advancedOptionsOpen, setAdvancedOptionsOpen] = useState(false);
     var [loading, setLoading] = useState(true);
 
 
@@ -37,12 +41,27 @@ const ValueSearch = () => {
         <div>
             <Navbar />
             <div className="container page-content text-center">
+
                 <div className="col-md-12 mt-2 pt-1 pb-1">
                     <h2>Value Search</h2>
-                    <button className="btn btn-sm mt-1 mb-1" onClick={fetchAllQuotes}>Run Value Search</button>
-                    <div className="row justify-content-center">
-                        <a className="mt-1 mb-1" href="#" onClick={refreshIEXCloudSymbols}>Refresh IEX Cloud Symbols</a>
+                    <div class="accordion" id="accordionExample">
+                        <div>
+                            <a class="text-center" href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne" onClick={advancedOptionsOpen === false ? () => {setAdvancedOptionsOpen(advancedOptionsOpen => true)} : () => {setAdvancedOptionsOpen(advancedOptionsOpen => false)}}>
+                                Advanced Options {advancedOptionsOpen === true ? <img className="text-icon" src={expandLessIcon} alt="expandLessIcon"/>:<img className="text-icon" src={expandMoreIcon} alt="expandMoreIcon"/>}
+                                </a>
+                            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                                <div className="card mt-1">
+                                    <div class="card-body">
+                                        <button className="btn btn-sm mt-1 mb-1" onClick={fetchAllQuotes}>Run Value Search</button>
+                                        <div className="row justify-content-center">
+                                            <a className="mt-1 mb-1" href="#" onClick={refreshIEXCloudSymbols}>Refresh IEX Cloud Symbols</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
             </div>
             <AuthTimeoutModal />
