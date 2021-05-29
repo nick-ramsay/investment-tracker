dc2fdc4b005e5ec3fcd2c884625af5661b210daf
Nick Ramsay <NickRamsay1989@gmail.com>
2021-05-29 20:07:02 +1000

    cleaned up formatting of navbar, error, and noaccess pages

--- client/src/pages/NoAccess/NoAccess.js
+++ client/src/pages/NoAccess/NoAccess.js
@@ -6,7 +6,7 @@ const NoAccess = () => {
     return (
         <div>
             <div className="container pt-4">
-                <div className="col-md-12 my-5 px-5 bg-white rounded content-container">
+                <div className="col-md-12 my-5 px-5 rounded content-container">
                     <div className="row text-center">
                         <div className="col-md-12 card">
                             <h1 className="mt-5" style={{ fontSize: 50 }}>
@@ -15,14 +15,14 @@ const NoAccess = () => {
                                     <span style={{ color: "green" }}>\</span>
                                     <span style={{ color: "red" }}>_</span>
                                     <span style={{ color: "purple" }}>(</span>
-                                    <span style={{ color: "white" }}>ツ</span>
+                                    <span style={{ color: "black" }}>ツ</span>
                                     <span style={{ color: "teal" }}>)</span>
                                     <span style={{ color: "gold" }}>_</span>
                                     <span style={{ color: "orange" }}>/</span>
                                     <span style={{ color: "blue" }}>¯</span>
                                 </strong>
                             </h1>
-                            <h1 style={{ fontSize: 100, color: "white" }}><strong>401</strong></h1>
+                            <h1 style={{ fontSize: 100, color: "black" }}><strong>401</strong></h1>
                             <h1 style={{ fontSize: 50, color: "red" }}><strong>None Shall Pass</strong></h1>
                             <h3 className="mb-5">Looks like you're not logged in. Log in and try again.</h3>
                         </div>
